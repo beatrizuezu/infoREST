7177afd999475040420712737b461a2f6fd43b3d
Beatriz Uezu <beatriz.uezu@gmail.com>
2017-03-01 20:57:49 -0300

    ajuste

--- fbInfo/models.py
+++ fbInfo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class UserFb(models.Model):
+    username = models.CharField(max_length=128)
+    name = models.CharField(max_length=256)
+    fb_id = models.CharField(max_length=128, unique=True)
+    gender = models.CharField(max_length=8)
