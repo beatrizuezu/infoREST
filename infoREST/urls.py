16e73e26feffa5c089598b51279f5d4530955760
Beatriz Uezu <beatriz.uezu@gmail.com>
2017-03-01 20:59:36 -0300

    namespace

--- infoREST/urls.py
+++ infoREST/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from fbInfo import views
+
+router = routers.DefaultRouter()
+router.register(r'UserFb', views.UserViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('fbInfo.urls'))
+    url(r'^api/', include('fbInfo.urls', namespace='rest_framework'))
 ]
