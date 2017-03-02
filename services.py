39c812a861ca21d825c27faafaba57e2254d486b
Beatriz Uezu <beatriz.uezu@gmail.com>
2017-03-02 01:36:18 -0300

    ajuste

--- services.py
+++ services.py
@@ -24,6 +24,12 @@ def obter_dados_userFb(fb_id):
     token = json['access_token']
     graph = fb.GraphAPI(access_token=token, version='2.7')
     dados = graph.get_object(fb_id, fields='gender,first_name,last_name')
-    return dados
 
+    data = {
+        'fb_id': fb_id,
+        'name': dados.get('first_name', 'undefined'),
+        'username': dados.get('first_name', 'undefined'),
+        'gender': dados.get('gender', 'undefined'),
+    }
+    return data
 # fb_id = '562073097'
