6bc3259d4fa9aad501b35c72a6ed6d75fe169e9b
Beatriz Uezu <beatriz.uezu@gmail.com>
2017-03-01 22:40:59 -0300

    ajuste

--- fbInfo/serializers.py
+++ fbInfo/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from .models import UserFb
+from services import obter_dados_userFb
 
 
 class UserFbSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserFb
-        fields = ['username', 'name', 'fb_id', 'gender']
+        fields = ['fb_id']
+
+    def create(self, fb_id, validated_data):
+        dados = obter_dados_userFb(fb_id)
+        return dados(**validated_data)
