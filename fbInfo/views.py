2995e4a08fdb36919ff1adf27a32fe38af8253b4
Beatriz Uezu <beatriz.uezu@gmail.com>
2017-03-02 02:23:31 -0300

    listview

--- fbInfo/views.py
+++ fbInfo/views.py
@@ -1,4 +1,7 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import viewsets
+from rest_framework import status
 from fbInfo.models import UserFb
 from fbInfo.serializers import UserFbSerializer
 
@@ -6,3 +9,12 @@ from fbInfo.serializers import UserFbSerializer
 class UserFbViewSet(viewsets.ModelViewSet):
     queryset = UserFb.objects.all().order_by('-fb_id')
     serializer_class = UserFbSerializer
+
+
+class UserFbListView(APIView):
+    serializer_class = UserFbSerializer
+
+
+    def get(self, request, format=None):
+        serializer = self.serializer_class(UserFb.objects.all(), many=True)
+        return Response(serializer.data)
