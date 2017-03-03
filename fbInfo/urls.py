00ae1cfa6827ba48f0790042db8cffe788ddf77a
Beatriz <beatriz.uezu@gmail.com>
2017-03-03 15:52:04 -0300

    rename

--- fbInfo/urls.py
+++ fbInfo/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from fbInfo import views
 
 urlpatterns = [
-    url(r'^userFb/$', views.UserFbListView.as_view()),
-    url(r'^userFb/(?P<pk>[0-9]+)/$', views.UserFbDetail.as_view()),
+    url(r'^user_fb/$', views.UserFbListView.as_view()),
+    url(r'^user_fb/(?P<pk>[0-9]+)/$', views.UserFbDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
