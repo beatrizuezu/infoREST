d5d66587f2cc0ea7271ce701a8758a0aa9a7a52c
Beatriz <beatriz.uezu@gmail.com>
2017-03-03 18:27:45 -0300

    login test

--- fbInfo/tests.py
+++ fbInfo/tests.py
@@ -7,7 +7,9 @@ import json
 class UserFbTests(APITestCase):
 
     def test_post(self):
+
         data = {'fb_id': '562073097'}
+        self.client.login(username='admin', password='password123')
         response = self.client.post('/user_fb/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(UserFb.objects.count(), 1)
@@ -15,8 +17,10 @@ class UserFbTests(APITestCase):
 
     def test_get(self):
         response = self.client.get('/user_fb/')
-        self.assertEqual(len(response.json()), UserFb.objects.count())
+        self.client.login(username='admin', password='password123')
+        self.assertEqual(len(response.json()), 1)
 
     def test_delete(self):
         response = self.client.delete('/user_fb/562073097/')
+        self.client.login(username='admin', password='password123')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
